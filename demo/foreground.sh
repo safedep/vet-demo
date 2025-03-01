7883d6998f126bba9b1c9ee7e7c75b5e2ac8ba51
Abhisek Datta <abhisek.datta@gmail.com>
2025-03-01 16:16:25 +0530

    updated changes

--- demo/foreground.sh
+++ demo/foreground.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-echo "Installing SafeDep vet..."
-cd /tmp/
-wget https://github.com/safedep/vet/releases/download/v1.9.4/vet_Linux_x86_64.tar.gz
-tar -xvzf vet_Linux_x86_64.tar.gz
-chmod +x vet
-mv vet /usr/local/bin/
+echo "Welcome to SafeDep vet 🔍"
 
-echo "Time to scan for malware by vet 🔍"
